4e2a4f3755ed7d086e979ce042e6571d134877f7
Eduardo Arlé <eduardoarle@tauex.tau.ac.il>
2022-12-21 09:10:02 +0000

    version 1.0.11

--- inst/doc/Using_bRacatus.R
+++ inst/doc/Using_bRacatus.R
@@ -61,10 +61,10 @@ pts2 <- data.frame(species=pts2$species,
                      origin="GBIF")       # Running time: < 1s.
 
 extra_points <- data.frame(species="Phalanger orientalis",
-                           decimalLongitude=c(125.257,112.765,110.632,112.192,121.130,
-                                              142.607,126.877,164.761,109.036),
-                           decimalLatitude=c(8.261,2.396,-1.518,-7.821,-20.655,-13.639,
-                                             -17.904,-20.671,12.938),
+                    decimalLongitude=c(125.257,112.765,110.632,112.192,121.130,
+                                       142.607,126.877,164.761,109.036),
+                    decimalLatitude=c(8.261,2.396,-1.518,-7.821,-20.655,-13.639,
+                                      -17.904,-20.671,12.938),
                            origin="Simulated")     # Running time: < 1s.
 
 pts3 <- rbind(pts2,extra_points)     # Running time: < 1s.
@@ -77,7 +77,8 @@ range_map_ref_reg <- rangeMaps(ref_reg)  # Running time: < 1s.
 plotRefReg (range_map_ref_reg)  # Running time: < 1s.
 
 ## ---- eval = FALSE------------------------------------------------------------
-#  signals_3 <- signalCalculation (range_map_ref_reg,pts3,biogeo = TRUE)   # Running time: ≈ 40s.
+#  signals_3 <- signalCalculation (range_map_ref_reg,pts3,biogeo = TRUE)
+#  # Running time: ≈ 40s.
 
 ## ---- echo = FALSE------------------------------------------------------------
 data(signals_3)
@@ -87,8 +88,14 @@ acc <- accuracy (signals_3)   # Running time: < 1s.
 biogeo <- biogeoStatus (signals_3)   # Running time: < 1s.
 
 ## -----------------------------------------------------------------------------
-plotAccuracy (acc, regional=T, reg.by="points", borders=F, col.features="gray80", col.bg="white", plot.range=T, range=Range_Phalanger_orientalis, box=T)   # Running time: < 1s.
+plotAccuracy (acc, regional=T, reg.by="points", borders=F, 
+              col.features="gray80",col.bg="white",plot.range=T,
+              range=Range_Phalanger_orientalis, box=T)
+# Running time: < 1s.
 
 ## -----------------------------------------------------------------------------
-plotBiogeoStatus (biogeo, regional=T, reg.by="points", borders=F, col.features="gray80", col.bg="white", plot.range=T, range=Range_Phalanger_orientalis, box=T)   # Running time: < 1s.
+plotBiogeoStatus (biogeo, regional=T, reg.by="points", borders=F,
+                  col.features="gray80", col.bg="white", plot.range=T, 
+                  range=Range_Phalanger_orientalis, box=T)
+# Running time: < 1s.
 
